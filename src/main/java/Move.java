cdeb1da2d8d310c742cc6fd73799dcb4bc6b31ab
Aryav-T <113959123+Aryav-T@users.noreply.github.com>
2024-04-16 09:45:02 +0530

    Create Move.java

---
+++ src/main/java/Move.java
@@ -0,0 +1,26 @@
+public class Move {
+   int rowMove;
+   int colMove;
+
+   public Move(int rowMove, int colMove) {
+       this.rowMove = rowMove;
+       this.colMove = colMove;
+   }
+
+   public int getRowMove() {
+       return rowMove;
+   }
+
+   public void setRowMove(int rowMove) {
+       this.rowMove = rowMove;
+   }
+
+   public int getColMove() {
+       return colMove;
+   }
+
+   public void setColMove(int colMove) {
+       this.colMove = colMove;
+   }
+}
+
